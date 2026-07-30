94f9000e84030fead84ba6abc59980d9984a3b5a
Mohammadabuaisheh <mohammadabuaisheh05@gmail.com>
2026-07-30 12:17:32 +0300

    The project structure and server file have been prepared

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("السيرفر شغال بنجاح! بانتظار الواجهات من الفريق.");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
