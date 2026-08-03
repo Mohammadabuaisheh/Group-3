8c356d82bc5150bb6bc2b3cea3cd791fe1e254d2
Mohammadabuaisheh <mohammadabuaisheh05@gmail.com>
2026-08-03 20:07:28 +0300

    Upadte app.js integrate slide requirements

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const indexRoutes = require('./routes/index');
+const indexRoutes = require("./routes/index");
 app.use('/', indexRoutes);
 
 app.use((req, res) => {
     res.status(404).render('404');
 });
 
-app.listen(port, () => {
+app.listen(port, ()=> {
     console.log(`SafePaws server running on http://localhost:${port}`);
 });
