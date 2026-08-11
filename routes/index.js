0aca3a11cea4d9ae6766d743cd6081efb64599fa
Mohammadabuaisheh <mohammadabuaisheh05@gmail.com>
2026-08-11 15:48:41 +0300

    Configure Phase 2 packages, sessions, and cookies

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,11 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
+const indexController = require('../controllers/indexController');
 
-router.get('/', (req, res) => {
-    res.render('home');
-});
-
-router.get('/about', (req, res) => {
-    res.render('about');
-});
-
-router.get('/features', (req, res) => {
-    const safePawsServices = [
-        { 
-            name: 'Adoption', 
-            description: 'Providing a reliable environment that allows users to browse animal details to easily find them a suitable shelter.' 
-        },
-        { 
-            name: 'Reporting', 
-            description: 'A rapid reporting system for lost animals or those requiring urgent medical intervention.' 
-        },
-        { 
-            name: 'Donation', 
-            description: 'A transparent channel to collect financial support to cover the costs of medical treatment and care.' 
-        }
-    ];
-
-    res.render('features', { services: safePawsServices });
-});
-
-router.get('/contact', (req, res) => {
-    res.render('contact'); 
-});
-
-router.get('/animal/:id', (req, res) => {
-    const animalId = req.params.id;
-
-    res.render('detail', {id: animalId});
-});
+router.get('/', indexController.getHome);
+router.get('/about', indexController.getAbout);
+router.get('/features', indexController.getFeatures);
+router.get('/contact', indexController.getContact);
+router.get('/animal/:id', indexController.getAnimalDetails);
 
 module.exports = router;
